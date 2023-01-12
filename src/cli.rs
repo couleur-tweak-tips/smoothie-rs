fef116ad52173bbc3e08f51f675c375142fa64f6
Anima <18208134+animafps@users.noreply.github.com>
2023-01-13 09:16:11 +1300

    fixed recipe string lifetime

--- src/cli.rs
+++ src/cli.rs
@@ -1,37 +1,38 @@
-use std::path::PathBuf;
 use clap::Parser;
+use std::path::PathBuf;
 
-/// Smoothen up your gameplay footage with Smoothie, yum! 
+/// Smoothen up your gameplay footage with Smoothie, yum!
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = "h")]
 pub struct Arguments {
-    /// Input video file paths
-    pub input:      Option<Vec<PathBuf>>,
 
     /// Payload containing video timecodes, used by NLE scripts
-    pub json:       Option<String>,
+    pub json: Option<String>,
 
     /// Output video filepath (one only)
     #[clap(short, long)]
-    pub output:     Option<String>,
+    pub output: Option<String>,
 
     /// Display extra "advanced" information, what I personally use
     #[clap(short, long, default_value_t = false)]
-    pub verb:       bool,
+    pub verb: bool,
 
     /// Join all cuts to a file, used with -json
     #[clap(short, long, default_value_t = false)]
-    pub trim:       bool,
+    pub trim: bool,
 
     /// Split all cuts to separate files, used with -json
     #[clap(short, long, default_value_t = false)]
-    pub split:      bool,
+    pub split: bool,
 
-    /// 
+    ///
     #[clap(short, long, default_value_t = false)]
-    pub debug:      bool,
+    pub debug: bool,
 
     /// Specify a recipe path (defaults to recipe.yaml)
     #[clap(short, long, default_value = "recipe.ini")]
-    pub recipe:     String,
+    pub recipe: String,
+
+    /// Input video file paths
+    pub input: Option<Vec<PathBuf>>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // #[macro_use]
-extern crate serde_derive;
 extern crate serde;
+extern crate serde_derive;
 extern crate serde_json;
 
 use clap::Parser;
@@ -13,12 +13,11 @@ mod recipe;
 
 fn main() {
     color_eyre::install().unwrap();
-    
+
     let _args = cli::Arguments::parse();
 
     // let rc: HashMap <String, String> = HashMap::new();
-    let _rc = recipe::get_recipe(&_args); 
+    let _rc = recipe::get_recipe(&_args);
     // let _videos = video::resolve_input(rc, _args);
     // exec::smoothing(videos);
-    
 }
