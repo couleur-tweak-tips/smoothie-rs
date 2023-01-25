2d5aacf719ba4044c2619fe3da199043e441b794
Couleur <82747632+couleurm@users.noreply.github.com>
2023-01-25 23:08:54 +0100

    remove recipe struct, refactor parse_recipe

--- src/recipe.rs
+++ src/recipe.rs
@@ -1,94 +1,49 @@
 use crate::cli::Arguments;
-
+#[allow(unused_imports)]
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+
+use std::path::Path;
+#[allow(unused_imports)]
 use std::path::PathBuf;
 use std::{env, fs};
 
 use std::io::Read;
 
-#[allow(non_camel_case_types)]
-#[derive(Debug, Serialize, Deserialize)]
-pub enum _Recipe {
-    interpolation {
-        enabled: bool,
-        fps: i32,
-        speed: String,
-        tuning: String,
-        algorithm: i32,
-        use_gpu: bool,
-    },
+type Recipe = HashMap<String, HashMap<String, String>>;
 
-    frame_blending {
-        enabled: bool,
-        fps: i32,
-        intensity: f64,
-        weighting: String,
-    },
+fn parse_recipe(ini: PathBuf, rc: &mut Recipe) {
+    assert!(ini.exists(), "Recipe at path `{:?}` does not exist", ini);
+    println!("recipe: {:?}", ini);
 
-    flowblur {
-        enabled: bool,
-        amount: i32,
-        mask: Option<PathBuf>,
-    },
+    let mut file = match fs::File::open(&ini) {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening file: {}", e),
+    };
 
-    encoding {
-        process: PathBuf,
-        loglevel: String,
-        args: String,
-    },
+    let metadata = match file.metadata() {
+        Ok(metadata) => metadata,
+        Err(e) => panic!("Error getting file metadata: {}", e),
+    };
+    if metadata.len() == 0 {
+        panic!("Error: File is empty: {:?}", file);
+    }
 
-    preview {
-        enabled: bool,
-        ffmpeg: String,
-        process: PathBuf,
-        args: String,
-    },
+    let mut content = String::new();
+    match file.read_to_string(&mut content) {
+        Ok(_) => (),
+        Err(e) => panic!("Error reading file: {}", e),
+    };
 
-    misc {
-        mpv_bin: PathBuf,
-        stay_on_top: bool,
-
-        ding_after: i32,
-        folder: Option<PathBuf>,
-        container: String,
-        file_format: String,
-        debug: bool,
-        dedupthreshold: i32,
-    },
-
-    console_params {
-        ontop: bool,
-        borderless: bool,
-        width: i32,
-        height: i32,
-    },
-
-    timescale {
-        input: f64,
-        output: f64,
-    },
-
-    pre_interp {
-        enabled: bool,
-        factor: i32,
-        model: PathBuf,
-    },
-}
-
-fn parse_recipe(content: String) {
-    let mut rc: HashMap<String, HashMap<&str, &str>> = HashMap::new();
-
-    let mut round = 0;
     let mut cur_section = String::new();
-    let lines: Vec<&str> = content.split('\n').collect();
+    let mut round = 1;
 
-    for line in lines {
+    for mut cur in content.split('\n') {
+        cur = cur.trim();
         round += 1;
-        let cur = line.trim();
 
         match cur {
-            // a comment or empty line
+            // a empty line or comment, just like this one
             _comment
                 if cur.starts_with('#')
                     || cur.starts_with('/')
@@ -96,7 +51,7 @@ fn parse_recipe(content: String) {
                     || cur.starts_with(':')
                     || cur.is_empty() => {}
 
-            // e.g [frame interpolation]
+            // [frame interpolation]
             category if cur.starts_with('[') && cur.ends_with(']') => {
                 cur_section = category
                     .trim_matches(|c| c == '[' || c == ']')
@@ -110,9 +65,9 @@ fn parse_recipe(content: String) {
                 }
             }
 
-            // e.g weighting: gaussian
+            // weighting: gaussian
             setting if cur.contains(':') => {
-                if cur_section == String::new() {
+                if cur_section.is_empty() {
                     panic!(
                         "Recipe: Setting {:?} has no parent category, line {round}",
                         setting
@@ -124,17 +79,16 @@ fn parse_recipe(content: String) {
                     .expect("Recipe: Failed to split {setting}, line {round}");
 
                 rc.get_mut(&cur_section)
-                    .unwrap()
-                    .insert(key.trim(), value.trim());
+                    .expect("Failed to get section `{cur_section}`")
+                    .insert(key.trim().to_string(), value.trim().to_string());
             }
-
+            // forgot to put val into a comment!
             _ => panic!("Recipe: Failed to parse {:?}, line {round}", cur),
         }
     }
-    println!("{:?}", rc["frame blending"]["fps"]);
 }
 
-pub fn get_recipe(args: &Arguments) {
+pub fn get_recipe(args: &Arguments) -> Recipe {
     let exe = match env::current_exe() {
         Ok(exe) => exe,
         Err(e) => panic!("Could not resolve Smoothie's binary path: {}", e),
@@ -145,37 +99,12 @@ pub fn get_recipe(args: &Arguments) {
         None => panic!("Could not resolve Smoothie's binary directory `{:?}`", exe),
     };
 
-    let rc_path = bindir.join(args.recipe.clone());
+    let rc_path = bindir.join(&args.recipe);
 
-    assert!(
-        rc_path.exists(),
-        "Recipe at path `{:?}` does not exist",
-        rc_path
-    );
+    let mut rc: Recipe = HashMap::new();
 
-    println!("recipe: {:?}", rc_path);
-
-    // Open the file at the given path
-    let mut file = match fs::File::open(&rc_path) {
-        Ok(file) => file,
-        Err(e) => panic!("Error opening file: {}", e),
-    };
-
-    // Check if the file is empty
-    let metadata = match file.metadata() {
-        Ok(metadata) => metadata,
-        Err(e) => panic!("Error getting file metadata: {}", e),
-    };
-    if metadata.len() == 0 {
-        panic!("Error: File is empty");
-    }
-
-    // Read the contents of the file into a string
-    let mut content = String::new();
-    match file.read_to_string(&mut content) {
-        Ok(_) => (),
-        Err(e) => panic!("Error reading file: {}", e),
-    };
-
-    parse_recipe(content);
+    parse_recipe(Path::join(bindir, "defaults.ini"), &mut rc);
+    parse_recipe(rc_path, &mut rc);
+    println!("rc: {:?}", rc);
+    return rc
 }
