6e987d036c209d901d8fb6e544e18416eef63fc5
Couleur <82747632+couleurm@users.noreply.github.com>
2023-01-29 20:06:40 +0100

    rename structs, add ensure_dir, resolve_outpath, unfinished struct building

--- src/video.rs
+++ src/video.rs
@@ -1,28 +1,126 @@
-use crate::cli::Arguments;
+use crate::{cli::Arguments, recipe::Recipe};
+use ffprobe::FfProbe;
+use rand::seq::SliceRandom;
 // use serde_json::Result;
+// select random fruit suffix
 use std::{collections::HashMap, fs, path::PathBuf};
 
-pub struct QueueObject {
-    videos: Vec<VideoObject>,
+#[derive(Debug, Clone)]
+pub struct Payload {
+    videos: Vec<Source>,
     outpath: String,
 }
-
-pub struct VideoObject {
-    path: PathBuf,                  // D:\obs stuff\video.mp4
-    basename: String,               // video.mp4
-    probe: HashMap<String, String>, // provided by ffprobe
-    timecodes: Vec<Timecodes>,
+#[derive(Debug, Clone)]
+pub struct Source {
+    path: PathBuf,    // D:\obs stuff\video.mp4
+    basename: String, // video
+    probe: FfProbe,   // provided by ffprobe
+    timecodes: Option<Vec<Timecodes>>,
+    // bettertimecodes: Option<Vec<(String, String)>>
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone, PartialEq, Hash)]
 struct Timecodes {
     filename: String,
     fin: String,
     start: String,
 }
 
+fn ensure_dir(dir: &PathBuf, silent: bool) {
+    if !dir.is_dir() {
+        match fs::create_dir(dir) {
+            Ok(_) => {
+                if !silent {
+                    println!("Creating folder `{:?}`", dir)
+                }
+            }
+            Err(e) => panic!("Failed creating folder at `{:?}`, Error: {e}", dir),
+        }
+    }
+}
+
+fn probe_input(input: Vec<PathBuf>) -> HashMap<PathBuf, FfProbe> {
+    let mut ret: HashMap<PathBuf, FfProbe> = HashMap::new();
+    for vid in &input {
+        let probe = match ffprobe::ffprobe(vid) {
+            Ok(a) => a,
+            Err(e) => {
+                println!("Skipping input file `{:?}` (failed probing): {:?}", &vid, e);
+                continue;
+            }
+        };
+        ret.insert(vid.to_path_buf(), probe);
+    }
+    ret
+}
+
+pub fn _resolve_outpath(
+    args: &mut Arguments,
+    rc: &mut Recipe,
+    indir: PathBuf,
+    basename: String,
+) -> PathBuf {
+    if args.output.is_some() {}
+
+    let outdir = if let Some(outdir_arg) = args.outdir.clone() {
+        println!("Using output directory `{:?}`", outdir_arg);
+        ensure_dir(&outdir_arg, false);
+        outdir_arg
+    } else if rc["misc"]["folder"].trim() != String::new() {
+        let outdir_recipe = PathBuf::from(rc["misc"]["folder"].clone());
+        ensure_dir(&outdir_recipe, false);
+        outdir_recipe
+    } else {
+        indir
+    };
+
+    let container: String = if rc["misc"]["container"].trim() == String::new() {
+        println!("Defaulting output extension to .MP4");
+        String::from("MP4")
+    } else {
+        rc["misc"]["container"].replace(".", "").to_string()
+    };
+
+    let mut filename = String::from("");
+
+    if !rc["misc"]["prefix"].is_empty() {
+        filename.push_str(&rc["misc"]["prefix"].clone());
+    }
+
+    filename.push_str(&basename);
+
+    #[rustfmt::skip]
+    let fruits: Vec<&str> = [
+        " Berry",      " Cherry",   " Cranberry",   " Coconut",   " Kiwi",
+        " Avocado",    " Durian",   " Lemon",       " Fig",       " Lime",
+        " Mirabelle",  " Banana",   " Pineapple",   " Pitaya",    " Blueberry",
+        " Raspberry",  " Apricot",  " Strawberry",  " Melon",     " Papaya",
+        " Apple",      " Pear",     " Orange",      " Mango",     " Plum",
+        " Peach",      " Grape",    " Tomato",      " Cucumber",  " Eggplant",
+        " Guava",      " Honeydew", " Lychee",      " Nut",       " Quince",
+        " Olive",      " Passion",  " Plum",        " Pomelo",    " Raisin",
+    ]
+    .to_vec();
+
+    let suffix: &str = &rc["misc"]["suffix"].clone();
+    // lmk if u manage to make it work without converting to a &str
+    let suffix: &str = match suffix {
+        "fruits" => fruits
+            .choose(&mut rand::thread_rng())
+            .expect("Failed to select a random suffix"),
+        _ => " ~ Smoothie",
+    };
+
+    filename.push_str(suffix);
+    filename.push_str(&format!(".{container}"));
+
+    return outdir.join(filename);
+}
+
 /// Attempts to resolve and structure input structs from CLI arguments
-pub fn resolve_input(mut args: Arguments) {
+pub fn resolve_input(args: &mut Arguments, _rc: &mut Recipe) -> Vec<Payload> {
+    let mut payloads: Vec<Payload> = vec![];
+
     if args.input.is_empty() && args.json.is_none() && args.tui == true {
         use rfd::FileDialog;
 
@@ -47,7 +145,6 @@ pub fn resolve_input(mut args: Arguments) {
 
     if !args.input.is_empty() {
         let mut input: Vec<PathBuf> = Vec::new();
-        // create a temporary vector (array)
 
         for vid in &args.input {
             // Get absolute form of the path (e.g .\video.mp4 -> C:\obs\videos.mp4)
@@ -76,7 +173,7 @@ pub fn resolve_input(mut args: Arguments) {
             if metadata.len() == 0 {
                 println!(
                     "{:?} is an empty file (0 bytes), discarding..",
-                    path.file_name().unwrap()
+                    path.file_name().expect("Failing getting input filename")
                 );
                 continue;
             }
@@ -94,6 +191,45 @@ pub fn resolve_input(mut args: Arguments) {
 
         args.input = input;
         // replace input with clean output
+
+        for vid in args.input.clone() {
+            let probe: FfProbe = match ffprobe::ffprobe(&vid) {
+                Ok(info) => info,
+                Err(err) => {
+                    panic!("Could not analyze file with ffprobe: {:?}", err);
+                }
+            };
+
+            dbg!(&probe);
+
+            payloads.push(Payload {
+                videos: vec![Source {
+                    path: vid.clone(),
+                    basename: vid
+                        .file_stem()
+                        .expect("Failed getting input filename's base name (stem)")
+                        .to_str()
+                        .expect("Failed converting input filename stem to &str")
+                        .to_string(),
+                    probe,
+                    timecodes: None,
+                }],
+
+                outpath: _resolve_outpath(
+                    args,
+                    _rc,
+                    vid.parent().unwrap().to_path_buf(),
+                    vid.file_stem()
+                        .expect("Failed getting filename base name (stem) when resolving output")
+                        .to_str()
+                        .expect("Failed converting")
+                        .to_string(),
+                )
+                .display()
+                .to_string(),
+            })
+        }
+        // dbg!(&payloads);
     }
     // if args.input is not
     else if args.json.is_some() {
@@ -103,13 +239,57 @@ pub fn resolve_input(mut args: Arguments) {
         };
 
         let _cuts: Vec<Timecodes> = serde_json::from_str(&args.json.clone().unwrap()).unwrap();
-        println!("{:?}", _cuts);
-        for cut in &_cuts {
-            println!("filename: {:?}", cut.filename);
-            println!("start: {:?}", cut.start);
-            println!("fin: {:?}", cut.fin);
+
+        let mut aggregated_cuts: HashMap<String, Vec<(String, String)>> = HashMap::new();
+
+        for cut in _cuts.clone() {
+            if !aggregated_cuts.contains_key(&cut.filename) {
+                aggregated_cuts.insert(cut.filename.clone(), vec![(cut.start, cut.fin)]);
+            } else {
+                aggregated_cuts
+                    .get_mut(&cut.filename)
+                    .unwrap()
+                    .append(&mut vec![(cut.start, cut.fin)]);
+            }
         }
-    }
+
+        dbg!(&aggregated_cuts);
+        let mut _sources: Vec<Source> = vec![];
+        // for cut in aggregated_cuts.keys() {
+        //     sources.push(
+        //         Source {
+        //             path: cut.into(),
+        //             basename: cut.
+        //             probe: (),
+        //             timecodes: () }
+        //     )
+        //     // payloads.push(
+        //     //     Payload {
+
+        //     //     }
+        //     // );
+        // }
+
+        // let mut _payload = Payload {
+        //     outpath: String::from("C:\\yay~SM.mp4"),
+        //     videos: vec![Source {
+        //         path: PathBuf::from("C:\\yay.mp4"),
+        //         basename: String::from("yay"),
+        //         probe: ffprobe::ffprobe("C:\\yay.mp4").unwrap(),
+        //         timecodes: None,
+        //     }],
+        // };
+        // // payload.outpath = String::from("hi");
+        // println!("{:?}", _cuts);
+        // for cut in &_cuts {
+        //     println!("filename: {:?}", cut.filename);
+        //     println!("start: {:?}", cut.start);
+        //     println!("fin: {:?}", cut.fin);
+        // }
+    };
+
+    payloads
+
     // }else {
     //     println!("ARGS: {:?}", args);
     //     println!("JSON: {:?}, INPUT: {:?}", !args.json.is_none(), !args.input.is_empty());
