c9e78193002c4f03278a0a1aa66ada70db50b16b
Anima <18208134+animafps@users.noreply.github.com>
2023-02-08 23:49:37 +1300

    finished change_fps function

--- src/vapoursynth/havsfunc.rs
+++ src/vapoursynth/havsfunc.rs
@@ -1,23 +1,27 @@
 use rustsynth::{
     core::CoreRef,
     function::Function,
-    map::OwnedMap,
     node::Node,
     owned_map,
     prelude::{Map, API},
 };
 
-pub fn change_fps<'core, 'elem>(
+pub fn change_fps<'elem ,'core: 'elem>(
+    api: API,
     core: CoreRef<'core>,
     clip: Node<'core>,
     fpsnum: f64,
     fpsden: f64,
-) -> OwnedMap<'elem> {
+) -> Node<'elem> {
     let factor = (fpsnum / fpsden)
         * (clip.video_info().unwrap().fps_den as f64 / clip.video_info().unwrap().fps_num as f64);
     let adjust_frame = Function::new(core, adjust_frame);
     let std = core.plugin_by_namespace("std").unwrap();
-    todo!()
+    let in_args = owned_map!(api, {"length": &(clip.video_info().unwrap().num_frames as f64 * factor).floor()}, {"fpsnum": &fpsnum}, {"fpsden": &fpsden});
+    let blank = std.invoke("BlankClip", &in_args);
+    let in_args = owned_map!(api, {"eval": &adjust_frame}, {"clip": &blank.get_node("clip").unwrap()});
+    let eval = std.invoke("FrameEval", &in_args);
+    eval.get_node("clip").unwrap()
 }
 
 fn adjust_frame<'core>(api: API, core: CoreRef<'core>, in_map: &Map<'core>, out: &mut Map<'core>) {
