97586e5c35f93a6f2883d192bb7953627de86272
Couleur <82747632+couleurm@users.noreply.github.com>
2023-02-12 18:33:11 +0100

    render.rs: fmt

--- src/render.rs
+++ src/render.rs
@@ -1,35 +1,39 @@
-use std::any::Any;
-use std::path::PathBuf;
 use crate::cmd::SmCommand;
 use crate::vapoursynth::output::{output, OutputParameters};
 use rustsynth::{
+    api::{CoreCreationFlags, API},
+    core::CoreRef,
     map::OwnedMap,
     node::Node,
-    api::{API, CoreCreationFlags},
-    core::CoreRef
 };
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
-fn libav_smashsource (filepath: PathBuf, core: CoreRef, api: API) -> Node {
-
+fn libav_smashsource(filepath: PathBuf, core: CoreRef, api: API) -> Node {
     let lsmas = core.plugin_by_namespace("lsmas").unwrap();
 
     let mut in_args = OwnedMap::new(api);
     in_args
-        .set_data("source", filepath.display().to_string().replace("\\\\?\\","").as_bytes())
+        .set_data(
+            "source",
+            filepath
+                .display()
+                .to_string()
+                .replace("\\\\?\\", "")
+                .as_bytes(),
+        )
         .expect("Failed setting input source parameter");
     let map = lsmas.invoke("LWLibavSource", &in_args);
 
-    map.get("clip").expect("Failed getting clip from LWLibavSource")
+    map.get("clip")
+        .expect("Failed getting clip from LWLibavSource")
 }
 
 pub fn vitamix(commands: Vec<SmCommand>) {
     let api = API::get().unwrap();
     let core = api.create_core(CoreCreationFlags::NONE);
 
-
     for cmd in commands {
-
         let clip = libav_smashsource(cmd.payload.in_path, core, api);
 
         let mut ffmpeg = Command::new("ffmpeg")
