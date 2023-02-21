6fd7094c7611979faa17f958aecf56441370ac1b
Couleur <82747632+couleurm@users.noreply.github.com>
2023-02-21 19:50:45 +0100

    move SetConsoleParams to utils.rs

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,11 @@ mod recipe;
 mod render;
 mod vapoursynth;
 mod video;
+mod utils;
+
 
 use crate::{cli::Arguments, cmd::SmCommand, recipe::Recipe, video::Payload};
 use std::env;
-use std::os::raw::c_int;
-
-extern "C" {
-    pub fn SetConsoleParams(
-        borderless: c_int,
-        always_on_top: c_int,
-        wnd_position: c_int,
-        wnd_cx: c_int,
-        wnd_cy: c_int,
-    );
-}
 
 fn main() {
     if enable_ansi_support::enable_ansi_support().is_err() {
@@ -46,54 +37,16 @@ fn main() {
     let recipe: Recipe = recipe::get_recipe(&args);
     // loads defaults.ini, then overrides recipe.ini over it
 
-    let is_conhost: bool = (env::var("WT_SESSION").is_err() && env::var("ALACRITY_LOG").is_err())
+    let is_conhost: bool = (env::var("WT_SESSION").is_err()
+        && env::var("ALACRITY_LOG").is_err())
         || env::var("NO_SMOOTHIE_WIN32").is_ok();
+    // user is neither running Windows Terminal and alacritty, OR has NO_SMOOTHIE_WIN32 defined
 
-    // if user has set an env var or is running windows terminal
     if is_conhost
         && cfg!(target_os = "windows")
         && !recipe.get_bool("miscellaneous", "always verbose")
     {
-        #[rustfmt::skip]
-        let pos = {
-            let pos = recipe.get("console", "position");
-
-            match pos.as_str() {
-                "top left"     | "top_left"     | "top-left"     | "topleft"     | "tl" => 0 as c_int,
-                "bottom left"  | "bottom_left"  | "bottom-left"  | "bottomleft"  | "bl" => 1 as c_int,
-                "top right"    | "top_right"    | "top-right"    | "topright"    | "tr" => 2 as c_int,
-                "bottom right" | "bottom_right" | "bottom-right" | "bottomright" | "br" => 3 as c_int,
-                _ => {
-                    println!("Unknown position `{:?}`, defaulting to `top left`", pos);
-                    0 as c_int
-                    }
-            }
-        };
-        unsafe {
-            SetConsoleParams(
-                recipe.get_bool("console", "borderless") as c_int,
-                recipe.get_bool("console", "stay on top") as c_int,
-                pos,
-                {
-                    match recipe.get("console", "width").parse::<c_int>() {
-                        Ok(height) => height,
-                        Err(_) => {
-                            println!("Failed parsing `[console] width:` to an integer, defaulting to 800");
-                            800 as c_int
-                        }
-                    }
-                },
-                {
-                    match recipe.get("console", "height").parse::<c_int>() {
-                        Ok(height) => height,
-                        Err(_) => {
-                            println!("Failed parsing `[console] height:` to an integer, defaulting to 600");
-                            600 as c_int
-                        }
-                    }
-                },
-            );
-        }
+        utils::set_window_position(&recipe);
     }
 
     let payloads: Vec<Payload> = video::resolve_input(&mut args, &recipe);
--- src/window.c
+++ src/window.c
@@ -1,69 +1,87 @@
-// by https://github.com/Aetopia
 #include <windows.h>
 
 BOOL SetConsoleWindowTitle(char *wndTitle) { return SetConsoleTitle(wndTitle); }
 
-BOOL SetConsoleParams(BOOL borderless, BOOL alwaysontop, int wnd_position, int wnd_cx, int wnd_cy)
+BOOL IsConsoleHost(BOOL bRelaunch)
 {
-    int wndX, wndY;
+    // Reference: https://stackoverflow.com/a/72575526
+    BOOL bConsoleHost = !GetWindow(GetConsoleWindow(), GW_OWNER);
+    if (!bConsoleHost && bRelaunch)
+    {
+        ShellExecute(0, "open", "conhost.exe", GetCommandLine(), ".", SW_SHOW);
+        exit(0);
+    };
+    return bConsoleHost;
+}
+
+BOOL SetConsoleParams(BOOL bBorderless, BOOL bAlwaysOnTop, int wndPos, int wndCX, int wndCY)
+{
+    if (!IsConsoleHost(FALSE))
+        return FALSE;
+    int wndX = 0,
+        wndY = 0;
+    UINT uFlags = 0;
     HWND hWnd = GetConsoleWindow(),
-            hWndPos = HWND_TOP;
+         hWndPos = HWND_TOP;
     DWORD wndStyle = WS_VISIBLE | WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_VSCROLL,
-            wndExStyle = WS_EX_ACCEPTFILES | WS_EX_WINDOWEDGE | WS_EX_APPWINDOW;
-    float scale = GetDpiForWindow(hWnd) / 96.0;
+          wndExStyle = WS_EX_ACCEPTFILES | WS_EX_WINDOWEDGE | WS_EX_APPWINDOW;
     HMONITOR hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-    UINT uFlags;
+
     MONITORINFO mi = {.cbSize = sizeof(MONITORINFO)};
+
     if (!hWnd ||
         !hMonitor ||
         !GetMonitorInfo(hMonitor, &mi) ||
-        !scale ||
         !ShowWindowAsync(hWnd, SW_NORMAL) ||
-        !SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE))
+        !SetWindowPos(hWnd,
+                      HWND_NOTOPMOST,
+                      0, 0, 0, 0,
+                      SWP_NOMOVE | SWP_NOSIZE))
         return FALSE;
-    if (!wnd_cx && !wnd_cy)
+
+    if (!wndCX || !wndCY)
         uFlags = SWP_NOSIZE;
-    if (alwaysontop)
+    if (bAlwaysOnTop)
         hWndPos = HWND_TOPMOST;
-    if (borderless)
+    if (bBorderless)
     {
         wndStyle = WS_VISIBLE | WS_POPUP;
         wndExStyle = WS_EX_APPWINDOW;
     };
 
-    switch (wnd_position)
+    switch (wndPos)
     {
-        // Top Left
-        case 0:
-            wndX = mi.rcWork.left;
-            wndY = mi.rcWork.top;
-            break;
+    // Top Left
+    case 1:
+        wndX = mi.rcWork.left;
+        wndY = mi.rcWork.top;
+        break;
 
-            // Top Right
-        case 1:
-            wndX = mi.rcWork.right - wnd_cx;
-            wndY = mi.rcWork.top;
-            break;
+        // Top Right
+    case 2:
+        wndX = mi.rcWork.right - wndCX;
+        wndY = mi.rcWork.top;
+        break;
 
-            // Bottom Left
-        case 2:
-            wndX = mi.rcWork.left;
-            wndY = mi.rcWork.bottom - wnd_cy;
-            break;
+        // Bottom Left
+    case 3:
+        wndX = mi.rcWork.left;
+        wndY = mi.rcWork.bottom - wndCY;
+        break;
 
-            // Bottom Right
-        case 3:
-            wndX = mi.rcWork.right - wnd_cx;
-            wndY = mi.rcWork.bottom - wnd_cy;
-            break;
+        // Bottom Right
+    case 4:
+        wndX = mi.rcWork.right - wndCX;
+        wndY = mi.rcWork.bottom - wndCY;
+        break;
 
-        default:
-            return FALSE;
+    default:
+        return FALSE;
     }
 
     SetWindowLong(hWnd, GWL_STYLE, wndStyle);
     SetWindowLong(hWnd, GWL_EXSTYLE, wndExStyle);
+    SetWindowPos(hWnd, hWndPos, wndX, wndY, wndCX, wndCY, uFlags);
 
-    SetWindowPos(hWnd, hWndPos, wndX, wndY, wnd_cx * scale, wnd_cy * scale, uFlags);
     return TRUE;
 }
