93e2b40baaef55998b3150753cb79c67631e9926
Couleur <82747632+couleurm@users.noreply.github.com>
2023-03-03 22:40:43 +0100

    use right dir

--- .github/workflows/shipper.ps1
+++ .github/workflows/shipper.ps1
@@ -5,7 +5,7 @@ mkdir ./smoothie-rs/bin/
 curl -L https://github.com/couleurm/VSBundler/releases/latest/download/VapourSynth.7z -o"vapoursynth.7z"
 7z x vapoursynth.7z -osmoothie-rs/bin/
 
-$env:VAPOURSYNTH_LIB_DIR=(Get-Item ./vapoursynth/sdk/lib64/).FullName
+$env:VAPOURSYNTH_LIB_DIR=(Get-Item ./smoothie-rs/bin/VapourSynth/sdk/lib64/).FullName
 cargo build --release
 
 cp ./target/release/smoothie-rs.exe ./smoothie-rs/bin/
