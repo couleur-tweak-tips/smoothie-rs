66c1816a3cd7b1da61077013faec0d7a40385b7e
Couleur <82747632+couleurm@users.noreply.github.com>
2023-03-03 22:58:16 +0100

    fix dirs

--- .github/workflows/shipper.ps1
+++ .github/workflows/shipper.ps1
@@ -4,6 +4,7 @@ mkdir ./smoothie-rs/bin/
 
 curl -L https://github.com/couleurm/VSBundler/releases/latest/download/VapourSynth.7z -o"vapoursynth.7z"
 7z x vapoursynth.7z -osmoothie-rs/bin/
+mv smoothie-rs/bin/VapourSynth/* -Recurse smoothie-rs/bin/
 
 $env:VAPOURSYNTH_LIB_DIR=(Get-Item ./smoothie-rs/bin/VapourSynth/sdk/lib64/).FullName
 cargo build --release
@@ -12,4 +13,4 @@ cp ./target/release/smoothie-rs.exe ./smoothie-rs/bin/
 cp ./target/scripts/*.py ./smoothie-rs/bin/VapourSynth
 cp ./target/*.ini ./smoothie-rs/
 
-set-content ./smoothie-rs-artifact/launch.cmd -value '@echo off & cd /D "%~dp0" & .\bin\smoothie-rs.exe --tui & pause'
+set-content ./smoothie-rs/launch.cmd -value '@echo off & cd /D "%~dp0" & .\bin\smoothie-rs.exe --tui & pause'
